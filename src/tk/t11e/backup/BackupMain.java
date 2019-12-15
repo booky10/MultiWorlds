05469ddfdf000bac53f27247da1ad1265bd96ba2
BuildTools <unconfigured@null.spigotmc.org>
2019-12-15 17:37:21 +0100

    Added Backup Command

--- src/tk/t11e/backup/BackupMain.java
+++ src/tk/t11e/backup/BackupMain.java
@@ -1,7 +1,7 @@
 package tk.t11e.backup;
 // Created by booky10 in MultiWorlds (15:15 15.12.19)
 
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitScheduler;
 import tk.t11e.backup.commands.Backup;
 import tk.t11e.multiworlds.main.Main;
@@ -12,50 +12,54 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
+@SuppressWarnings("ResultOfMethodCallIgnored")
 public class BackupMain {
 
-    private static Main main = Main.getPlugin(Main.class);
+    public static final String PREFIX = "§7[§bBackup§7]§c ", NO_PERMISSION = PREFIX + "You don't have " +
+            "the permissions for this!";
+    private static final Main main = Main.getPlugin(Main.class);
     private static File serverFolder, backupsFolder;
 
     public static void onStart() {
         serverFolder = new File(main.getDataFolder().getAbsolutePath() + "/../..");
         backupsFolder = new File(serverFolder.getAbsolutePath() + "/backups");
 
-        //initScheduler(Bukkit.getScheduler(), main);
-        //initCommands(main);
+        initScheduler(Bukkit.getScheduler());
+        initCommands();
     }
 
-    private static void initScheduler(BukkitScheduler scheduler, JavaPlugin plugin) {
-        scheduler.runTaskTimer(plugin, new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        }, 20, 20 * 30);
+    private static void initScheduler(BukkitScheduler scheduler) {
+        scheduler.runTaskTimer(BackupMain.main, () -> {
+            if (main.getConfig().getLong("lastBackup") + 36000000 < System.currentTimeMillis())
+                try {
+                    if (makeBackup())
+                        System.out.println("[Backup] Successfully made backup!");
+                    else
+                        System.out.println("[Backup] Something went wrong while making backup!");
+                } catch (IOException exception) {
+                    System.out.println(exception.getMessage());
+                }
+        }, 20 * 10, 20 * 60);
     }
 
-    private static void initCommands(JavaPlugin plugin) {
-        Objects.requireNonNull(plugin.getCommand("backup")).setExecutor(new Backup());
+    private static void initCommands() {
+        Objects.requireNonNull(BackupMain.main.getCommand("backup")).setExecutor(new Backup());
     }
 
-    public static long makeBackup() throws IOException {
+    public static boolean makeBackup() throws IOException {
         long time = System.currentTimeMillis();
         if (!backupsFolder.exists()) backupsFolder.mkdir();
+        boolean finished = true;
 
-        for (String worldName : Main.getWorldNames())
-            makeBackup(worldName);
-        return time;
-    }
+        for (String worldName : Main.getWorldNames()) {
+            File input = new File(serverFolder.getAbsolutePath() + "/" + worldName);
+            String backupName = worldName + "+" + TimeUtil.getStringTime(time) + "+" + time;
+            File output = new File(backupsFolder.getAbsolutePath() + "/" + backupName + ".zip");
 
-    public static long makeBackup(String worldName) throws IOException {
-        long time = System.currentTimeMillis();
-        if (!backupsFolder.exists()) backupsFolder.mkdir();
-
-        File input = new File(serverFolder.getAbsolutePath() + "/" + worldName);
-        String backupName = worldName + "+" + TimeUtil.getStringTime(time) + "+" + time;
-        File output = new File(backupsFolder.getAbsolutePath() + "/" + backupName + ".zip");
-
-        ZipUtil.zipFolder(input, output);
-        return time;
+            if (!ZipUtil.zipFolder(input, output)) finished = false;
+        }
+        main.getConfig().set("lastBackup", time);
+        main.saveConfig();
+        return finished;
     }
 }
--- src/tk/t11e/backup/commands/Backup.java
+++ src/tk/t11e/backup/commands/Backup.java
@@ -6,30 +6,60 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import tk.t11e.multiworlds.main.Main;
+import tk.t11e.backup.BackupMain;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("NullableProblems")
 public class Backup implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            if (player.hasPermission("")) {
-
-            } else
-                player.sendMessage(Main.NO_PERMISSION);
-        } else {
-
-        }
+            if (player.hasPermission("backup"))
+                if (args.length == 1)
+                    if (args[0].equalsIgnoreCase("backup"))
+                        try {
+                            if (BackupMain.makeBackup())
+                                player.sendMessage(BackupMain.PREFIX + "§aSuccessfully made backup!");
+                            else
+                                player.sendMessage(BackupMain.PREFIX + "Something went wrong while " +
+                                        "making backup!");
+                        } catch (IOException exception) {
+                            System.out.println(exception.getMessage());
+                            player.sendMessage(BackupMain.PREFIX + exception.getMessage());
+                        }
+                    else
+                        return false;
+                else
+                    return false;
+            else
+                player.sendMessage(BackupMain.NO_PERMISSION);
+        } else if (args.length == 1)
+            if (args[0].equalsIgnoreCase("backup"))
+                try {
+                    if (BackupMain.makeBackup())
+                        sender.sendMessage("Successfully made backup!");
+                    else
+                        sender.sendMessage("Something went wrong while making backup!");
+                } catch (IOException exception) {
+                    System.out.println(exception.getMessage());
+                    sender.sendMessage(exception.getMessage());
+                }
+            else
+                return false;
+        else
+            return false;
         return true;
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         List<String> list = new ArrayList<>();
+        list.add("backup");
         return list;
     }
 }
